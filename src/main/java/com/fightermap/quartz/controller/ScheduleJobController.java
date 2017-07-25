8ea920b46604f556aad396311157f544b61e9d9d
kilsoul <307898377@qq.com>
2017-07-25 13:48:10 +0800

    增加控制器

--- src/main/java/com/fightermap/quartz/controller/ScheduleJobController.java
+++ src/main/java/com/fightermap/quartz/controller/ScheduleJobController.java
@@ -5,6 +5,7 @@ import com.fightermap.quartz.service.ScheduleJobService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
@@ -18,7 +19,7 @@ public class ScheduleJobController {
     @Autowired
     private ScheduleJobService scheduleJobService;
 
-    @RequestMapping("/getAllTask")
+    @RequestMapping(value = "/getAllTask",method = RequestMethod.GET)
     public List<ScheduleJob> getAllTask(HttpServletRequest request){
         return scheduleJobService.getAllTask();
     }
--- src/main/java/com/fightermap/quartz/service/ScheduleJobService.java
+++ src/main/java/com/fightermap/quartz/service/ScheduleJobService.java
@@ -2,8 +2,13 @@ package com.fightermap.quartz.service;
 
 import com.fightermap.quartz.dao.ScheduleJobDao;
 import com.fightermap.quartz.model.ScheduleJob;
+import com.fightermap.quartz.task.QuartzJob;
+import org.quartz.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.util.List;
 
@@ -15,10 +20,52 @@ public class ScheduleJobService {
     @Resource
     private ScheduleJobDao scheduleJobDao;
 
+    @Autowired
+    private SchedulerFactoryBean schedulerFactoryBean;
+
     public ScheduleJobService() {
     }
 
     public List<ScheduleJob> getAllTask(){
         return scheduleJobDao.getAllTask();
     }
+
+    @PostConstruct
+    public void init() throws Exception{
+        Scheduler scheduler = schedulerFactoryBean.getScheduler();
+
+        List<ScheduleJob> jobList = scheduleJobDao.getAllTask();
+
+        for (ScheduleJob job : jobList){
+            addJob(job);
+        }
+    }
+
+    public void addJob(ScheduleJob job) throws Exception{
+        if (job == null || !"1".equals(job.getJobStatus())){
+            return;
+        }
+
+        Scheduler scheduler = schedulerFactoryBean.getScheduler();
+        TriggerKey triggerKey = TriggerKey.triggerKey(job.getJobName(),job.getJobGroup());
+
+        CronTrigger trigger = (CronTrigger) scheduler.getTrigger(triggerKey);
+
+        if (trigger == null){
+            Class clazz = QuartzJob.class;
+            JobDetail jobDetail = JobBuilder.newJob(clazz).withIdentity(job.getJobName(),job.getJobGroup()).build();
+            jobDetail.getJobDataMap().put("scheduleJob",job);
+
+            CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(job.getCronExpression());
+            trigger = TriggerBuilder.newTrigger().withIdentity(job.getJobName(),job.getJobGroup()).withSchedule(cronScheduleBuilder).build();
+
+            scheduler.scheduleJob(jobDetail,trigger);
+        }else {
+            CronScheduleBuilder cronScheduleBuilder = CronScheduleBuilder.cronSchedule(job.getCronExpression());
+
+            trigger = trigger.getTriggerBuilder().withIdentity(job.getJobName(),job.getJobGroup()).withSchedule(cronScheduleBuilder).build();
+            scheduler.rescheduleJob(triggerKey,trigger);
+        }
+    }
+
 }
